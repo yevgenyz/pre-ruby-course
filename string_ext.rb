e12517bcfb531b91a4ff4b06baa8b07cca4dd625
Oved Ourfali <oourfali@redhat.com>
2016-02-21 14:50:55 +0200

    add initialize contact and substract

--- string_ext.rb
+++ string_ext.rb
@@ -1,2 +1,17 @@
 class StringExt
+  left = ""
+  right = ""
+
+  def initialize (left, right)
+    @left = left;
+    @right = right;
+  end
+
+  def concat
+    @left + @right
+  end
+
+  def substract
+    @left.gsub(@right, '')
+  end
 end
