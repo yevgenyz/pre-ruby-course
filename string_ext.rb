c5aa802d6bd1fccb236e31cfa5ac6edf0ea40a0e
Yevgeny Zaspitsky <yzaspits@redhat.com>
2016-02-25 21:03:25 +0200

    First Assignment

--- string_ext.rb
+++ string_ext.rb
@@ -1,2 +1,14 @@
 class StringExt
+  def initialize(str1, str2)
+    @str1 = str1
+    @str2 = str2
+  end
+
+  def concat
+    @str1 + @str2
+  end
+
+  def substract
+    @str1.sub @str2, ''
+  end
 end
